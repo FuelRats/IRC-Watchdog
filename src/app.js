4452e404b2a2fe2ebb1d804e8f0c9ce3bd0fc9c9
Alex S. Glomsaas <alex@sorlie.co.uk>
2018-03-03 04:45:45 +0100

    Fix colour coding

--- src/app.js
+++ src/app.js
@@ -97,10 +97,10 @@ try {
         let [capture, nick, user, host, filter, message, target] = spamMatch
 
         if (filter === '*opsignal*' || filter === '*opssignal*') {
-          client.say('#opers', `${irc.colors.wrap('red', 'OPSIGNAL')} by ${nick} in channel/query ${target}. Original message: "${message}"`)
+          client.say('#opers', `${irc.colors.wrap('light_red', 'OPSIGNAL')} by ${nick} in channel/query ${target}. Original message: "${message}"`)
           client.textNotification(`OPSIGNAL by ${nick} in channel/query ${target}. Original message: ${message}`)
         } else {
-          client.say('#opers', `${irc.colors.wrap('red', 'SPAMFILTER')} triggered by ${nick} in channel/query ${target}.`)
+          client.say('#opers', `${irc.colors.wrap('light_red', 'SPAMFILTER')} triggered by ${nick} in channel/query ${target}.`)
         }
       }
       spamfilterMatch.lastIndex = 0
@@ -108,28 +108,28 @@ try {
       let killMatches = killMatch.exec(text)
       if (killMatches) {
         let [capture, nick, user, host, sender, message] = killMatches
-        client.say('#opers', `${irc.colors.wrap('red', 'KILL ')} ${nick} (${host}) was killed by ${sender} with message: ${message}`)
+        client.say('#opers', `${irc.colors.wrap('light_red', 'KILL ')} ${nick} (${host}) was killed by ${sender} with message: ${message}`)
       }
       killMatch.lastIndex = 0
 
       let operServMatches = operServMatch.exec(text)
       if (operServMatches) {
         let [capture, message] = operServMatches
-        client.say('#opers', `${irc.colors.wrap('red', 'OperServ ')} ${message}`)
+        client.say('#opers', `${irc.colors.wrap('light_red', 'OperServ ')} ${message}`)
       }
       operServMatch.lastIndex = 0
 
       let expireMatches = expireMatch.exec(text)
       if (expireMatches) {
         let [capture, message] = expireMatches
-        client.say('#opers', `${irc.colors.wrap('red', 'Expiring ')} ${message}`)
+        client.say('#opers', `${irc.colors.wrap('light_red', 'Expiring ')} ${message}`)
       }
       expireMatch.lastIndex = 0
 
       let banMatches = banMatch.exec(text)
       if (banMatches) {
         let [message] = banMatches
-        client.say('#opers', `${irc.colors.wrap('red', 'NETWORK BAN ')} ${message}`)
+        client.say('#opers', `${irc.colors.wrap('light_red', 'NETWORK BAN ')} ${message}`)
       }
       expireMatch.lastIndex = 0
     }
